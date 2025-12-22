1c6e24c2e9c1d41deca2431a7cd8a1ff3fac645d
asirago <40822894+asirago@users.noreply.github.com>
2025-12-22 06:47:37 +0100

    feat(message): implement BitTorrent message read/write
    - add message struct with ID and Payload fields
    - implement ReadMessage and WriteMessage

--- message.go
+++ message.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+	"net"
+)
+
 type MessageType uint8
 
 const (
@@ -36,4 +43,47 @@ func (bf *Bitfield) HasPiece(index int64) bool {
 	byteOffset := index % 8
 
 	return (*bf)[byteIndex]&(1<<(7-byteOffset)) != 0
+}
+
+type Message struct {
+	ID      MessageType
+	Payload []byte
+}
+
+func WriteMessage(conn net.Conn, msg *Message) error {
+	var buf bytes.Buffer
+
+	length := uint32(1 + len(msg.Payload))
+	binary.Write(&buf, binary.BigEndian, length)
+	buf.WriteByte(byte(msg.ID))
+	buf.Write(msg.Payload)
+
+	_, err := conn.Write(buf.Bytes())
+	return err
+}
+
+func ReadMessage(conn net.Conn) (*Message, error) {
+
+	lengthMsg := make([]byte, 4)
+	_, err := conn.Read(lengthMsg)
+	if err != nil {
+		return nil, err
+	}
+
+	length := binary.BigEndian.Uint32(lengthMsg)
+	if length == 0 {
+		return &Message{ID: 255}, nil
+	}
+
+	msgBuf := make([]byte, length)
+	_, err = io.ReadFull(conn, msgBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Message{
+		ID:      MessageType(msgBuf[0]),
+		Payload: msgBuf[1:],
+	}, nil
+
 }
