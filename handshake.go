75b198f4a0554366de735390f6b7d8539ce53548
asirago <40822894+asirago@users.noreply.github.com>
2025-12-22 06:51:21 +0100

    feat(handshake): add handshake reading and debug helpers

--- handshake.go
+++ handshake.go
@@ -1,26 +1,67 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+	"fmt"
+	"io"
+)
 
 type Handshake struct {
+	Length   uint8
 	Prefix   string
+	Reserved [8]byte
 	InfoHash [20]byte
 	PeerID   [20]byte
 }
 
 func NewHandshake(infoHash, peerID [20]byte) *Handshake {
 	return &Handshake{
+		Length:   byte(19),
 		Prefix:   "BitTorrent protocol",
+		Reserved: [8]byte{},
 		InfoHash: infoHash,
 		PeerID:   peerID,
 	}
 }
 
+func ReadHandshake(r io.Reader) (*Handshake, error) {
+	buf := make([]byte, 68)
+	_, err := io.ReadFull(r, buf)
+	if err != nil {
+		return nil, err
+	}
+
+	var reserved [8]byte
+	copy(reserved[:], buf[20:28])
+
+	var infoHash [20]byte
+	copy(infoHash[:], buf[28:48])
+
+	var peerID [20]byte
+	copy(peerID[:], buf[48:68])
+
+	return &Handshake{
+		Length:   buf[0],
+		Prefix:   string(buf[1:20]),
+		Reserved: reserved,
+		InfoHash: infoHash,
+		PeerID:   peerID,
+	}, nil
+}
+
+func (h *Handshake) PrintHandshake() {
+	fmt.Printf("Handshake received:\n")
+	fmt.Printf("  Protocol Length: \\x%02x\n", h.Length)
+	fmt.Printf("  Protocol: %s\n", h.Prefix)
+	fmt.Printf("  Reserved: %x\n", h.Reserved)
+	fmt.Printf("  Info Hash: %x\n", h.InfoHash)
+	fmt.Printf("  Peer ID: %s\n", h.PeerID)
+}
 func (h *Handshake) Serialize() []byte {
 	msg := &bytes.Buffer{}
-	msg.WriteByte(19)
+	msg.WriteByte(h.Length)
 	msg.Write([]byte(h.Prefix))
-	msg.Write(make([]byte, 8))
+	msg.Write(h.Reserved[:])
 	msg.Write(h.InfoHash[:])
 	msg.Write(h.PeerID[:])
 
