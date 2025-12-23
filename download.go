44985c4c4cd799d77e34ebceee418cc72c3e9024
asirago <40822894+asirago@users.noreply.github.com>
2025-12-23 22:12:45 +0100

    feat(download): add message handling to PieceProgress

--- download.go
+++ download.go
@@ -1,5 +1,7 @@
 package main
 
+import "log"
+
 type PieceProgress struct {
 	Index      uint32
 	PeerConn   *Connection
@@ -14,4 +16,32 @@ func NewPieceProgress(index uint32, length int64, conn *Connection) *PieceProgre
 		Buf:      make([]byte, length),
 		PeerConn: conn,
 	}
+}
+
+func (pp *PieceProgress) readMessage() error {
+	msg, err := ReadMessage(pp.PeerConn.Conn)
+	if err != nil {
+		return err
+	}
+
+	if msg == nil {
+		return nil
+	}
+
+	switch msg.ID {
+	case MsgUnchoke:
+		pp.PeerConn.Unchoked = true
+	case MsgChoke:
+		pp.PeerConn.Unchoked = false
+	case MsgPiece:
+		n, err := ParsePiece(pp.Index, pp.Buf, msg)
+		if err != nil {
+			return err
+		}
+		pp.Downloaded += n
+	default:
+		log.Printf("Received: Message ID %d\n", msg.ID)
+	}
+
+	return nil
 }
