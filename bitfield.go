28d8d314f14ea4ecb1ccd856447bccc435991ac8
asirago <40822894+asirago@users.noreply.github.com>
2025-12-23 23:39:13 +0100

    refactor(bitfield): change function parameter to int from int64
    - int is sufficient for representing piece indices
    - returning Bitfield pointer not necessary for []byte

--- bitfield.go
+++ bitfield.go
@@ -7,22 +7,22 @@ import (
 
 type Bitfield []byte
 
-func NewBitfield(numPieces int64) *Bitfield {
+func NewBitfield(numPieces int) Bitfield {
 	bitfieldSize := (numPieces + 7) / 8
 
 	bitfield := Bitfield(make([]byte, bitfieldSize))
 
-	return &bitfield
+	return bitfield
 }
 
-func (bf *Bitfield) SetPiece(index int64) {
+func (bf *Bitfield) SetPiece(index int) {
 	byteIndex := index / 8
 	byteOffset := index % 8
 
 	(*bf)[byteIndex] |= 1 << (7 - byteOffset)
 }
 
-func (bf *Bitfield) HasPiece(index int64) bool {
+func (bf *Bitfield) HasPiece(index int) bool {
 	byteIndex := index / 8
 	byteOffset := index % 8
 
